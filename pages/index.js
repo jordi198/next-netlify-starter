4bc004e9bf54242422f077a8667c04e6a73707c8
jordi198 <75859067+jordi198@users.noreply.github.com>
2022-08-09 13:23:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,6 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <iframe>
-          <code>
-            src="https://gateway.ipfscdn.io/ipfs/QmUfp6thZQTmNKS6tzijJpxdoBe9X7spHwzRjUh3RPTAwF/nft-drop.html?contract=0xF5C7C49d7736923Fc2aA54141c43b0ad7Ee16F28&chainId=137"
-            width="600px"
-            height="600px"
-            style="max-width:100%;"
-            frameborder="0"
-          </code>
-        </iframe>
       </main>
 
       <Footer />
